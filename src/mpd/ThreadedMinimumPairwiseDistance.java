71b10c364b9caafb5de73cdc0a29f30d83756f08
dragoteon <woest015@morris.umn.edu>
2017-12-19 00:41:17 -0600

    fixed the threading initialization issue....they are no longer all LL. Refactored with better variable names. and fixed the math issue in where C thread was checking. All tests pass

--- src/mpd/ThreadedMinimumPairwiseDistance.java
+++ src/mpd/ThreadedMinimumPairwiseDistance.java
@@ -3,25 +3,23 @@ package mpd;
 public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance {
 
 	//initializing Minimum to max value.
-	static int Minimum = Integer.MAX_VALUE;
+	public int Minimum = Integer.MAX_VALUE;
 	//function to determine if the current minimum is lower than the new minimum
-     public void areYouUpTodate (int resultsMaybe) {
+     public void updateGlobalResult (int resultsMaybe) {
         if(resultsMaybe < Minimum) {
                 Minimum = resultsMaybe;
-        } else {
-        //do nothing 
         }
     }    
 
     //setting up all the threads for minimumPairwiseDistance
 	@Override
-    public int minimumPairwiseDistance(int[] values) {
-       // throw new UnsupportedOperationException();
+	public int minimumPairwiseDistance(int[] values) {
     Thread LL = new Thread(new LL(values));
-	Thread BR = new Thread(new LL(values));
-	Thread TR = new Thread(new LL(values));
-	Thread C = new Thread(new LL(values));
+	Thread BR = new Thread(new BR(values));
+	Thread TR = new Thread(new TR(values));
+	Thread C = new Thread(new C(values));
 	//Starting all of the previous threads
+	
 	LL.start();
 	BR.start();
 	TR.start();
@@ -45,104 +43,100 @@ public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance 
     //
     //Create class for each thread
     //
+	//lMinimum is the local min for each thread so it can refer back to the overall min
+	
 	class LL implements Runnable {
 	       //create variable to store the newly found possible minimum
-	       int am_I_The_New_Minimum;
+	       int lMinimum = Integer.MAX_VALUE;
 	       int[] values;
 
 
-	LL(int minimum, int[] values) {
+	LL(int[] values) {
 		this.values = values;
-		am_I_The_New_Minimum = minimum;
 	}	
 	//Thread then runs the following nest for loops, looping over its area to find the minimum value of
 	//that area
 	//then checks the current global min to the found min, if the found min is lower it updates the global
 	//the area checked by the for loop changes for each thread.
+
 	public void run() {
-		int new_result_maybe = Integer.MAX_VALUE;
 		for (int i = 0; i < values. length/2; i++) {
     			for (int j = 0; j < i; j++) {
 				int difference = Math.abs(values[i] - values[j]);
-		    		if (difference < new_result_maybe) {
-			    		new_result_maybe = difference;
+		    		if (difference < lMinimum) {
+			    		lMinimum = difference;
 				    }
 		}
 
 	}
-	areYouUpTodate(new_result_maybe);
+	updateGlobalResult(lMinimum);
 
     }
 }
 	class BR implements Runnable {
-               int am_I_The_New_Minimum;
+			   int lMinimum = Integer.MAX_VALUE;
                int[] values;
-        BR(int minimum, int[] values) { 
-                this.values = values;
-                am_I_The_New_Minimum = minimum;
+       BR(int[] values) { 
+              this.values = values;
         }
+       
         public void run() {
-                int new_result_maybe = Integer.MAX_VALUE;
                 for (int i = values.length/2; i < values. length; i++) {                
-                        for (int j = 0; j < i - values.length/2; j++) {
+                        for (int j = 0; j < i - (values.length/2); j++) {
                                 int difference = Math.abs(values[i] - values[j]);
-                                if (difference < new_result_maybe) {
-                                        new_result_maybe = difference;
+                                if (difference < lMinimum) {
+                                        lMinimum = difference;
                                     }
                 }
                 
         }
-        areYouUpTodate(new_result_maybe);
+        updateGlobalResult(lMinimum);
     }
 
 }
 	class TR implements Runnable {
-               int am_I_The_New_Minimum;
+		       int lMinimum = Integer.MAX_VALUE;
                int[] values;
 
-        TR(int minimum, int[] values) { 
+        TR(int[] values) { 
                 this.values = values;
-                am_I_The_New_Minimum = minimum;
         }
-
+        
         public void run() {
-                int new_result_maybe = Integer.MAX_VALUE;
                 for (int i = values.length/2; i < values. length; i++) {                
                         for (int j = values.length/2; j < i; j++) {
                                 int difference = Math.abs(values[i] - values[j]);
-                                if (difference < new_result_maybe) {
-                                        new_result_maybe = difference;
+                                if (difference < lMinimum) {
+                                        lMinimum = difference;
                                     }
                 }
                 
         }
-        areYouUpTodate(new_result_maybe);
+        updateGlobalResult(lMinimum);
 
     }
 
 }
 
 	class C implements Runnable {
-               int am_I_The_New_Minimum;
+		       int lMinimum = Integer.MAX_VALUE;
                int[] values;
 
-        C(int minimum, int[] values) { 
+        C(int[] values) { 
                 this.values = values;
-                am_I_The_New_Minimum = minimum;
         }
-
+      
         public void run() {
-                int new_result_maybe = Integer.MAX_VALUE;
-                for (int i = 0; i < values. length/2; i++) {                
-                        for (int j = 0; j < i + values.length/2; j++) {
-                                int difference = Math.abs(values[j] - values[i]);
-                                if (difference < new_result_maybe) {
-                                        new_result_maybe = difference;
+                for (int i = values.length/2; i < values. length; i++) {                
+                        for (int j = i - (values.length/2);j < values.length/2; j++) {
+                                int difference = Math.abs(values[i] - values[j]);
+                                if (difference < lMinimum) {
+                                        lMinimum = difference;
                                     }
                 }
                 
         }
-        areYouUpTodate(new_result_maybe);
+        updateGlobalResult(lMinimum);
 
     }
 
